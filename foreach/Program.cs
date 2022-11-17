23fd272e9690e1fa4201251555a079f5002b8e7f
Carlos.Azcarraga <tasajos@gmail.com>
2022-11-17 14:45:46 -0400

    foreach

---
+++ foreach/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int [] nums = new int [] {1,7,9,22,56};
+
+foreach (int i in nums)
+{
+    Console.WriteLine("El valor es {0}", i);
+
+}
