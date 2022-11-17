affc747ad39079633f8ae5a8e5d83f3581bb1d78
Carlos.Azcarraga <tasajos@gmail.com>
2022-11-17 15:31:12 -0400

    while

---
+++ while/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name ="";
+
+Console.WriteLine ("Please input your name");
+
+while (name!="HOLA"){
+name = Console.ReadLine ();
+Console.WriteLine("your name is {0}", name );
+
+}
