ccfbf00bca11e719b1f1dfcaa8bfcc85f85ec5eb
Carlos.Azcarraga <tasajos@gmail.com>
2022-11-17 09:54:47 -0400

    https://github.com/tasajos/csharpt.git

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Nice");
+Console.WriteLine("What is your name");
+string str = Console.ReadLine();
+Console.WriteLine("Welcome " + str);
+
+
