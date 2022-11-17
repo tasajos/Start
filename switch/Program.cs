12f07423b17aa538af2b4dd42d9c855b56dc3b9e
Carlos.Azcarraga <tasajos@gmail.com>
2022-11-17 14:21:40 -0400

    switch

--- switch/Program.cs
+++ switch/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int a ;
+
+Console.WriteLine("Por favor ingrese un nro");
+
+
+/* Reading a line from the console and converting it to a long integer. */
+a = Convert.ToInt32 (Console.ReadLine());
+
+switch(a) {
+
+case 1:
+Console.WriteLine("Es menor a 5");
+break;
+case 2:
+Console.WriteLine("Es menor a 5");
+break;
+case 3:
+Console.WriteLine("Es menor a 5");
+break;
+case 4:
+Console.WriteLine("Es menor a 5");
+break;
+case 5:
+Console.WriteLine("Es menor a 5");
+break;
+case >5 and <10:
+Console.WriteLine("Es mayor a 5 y menor a 10");
+break;
+default:
+Console.WriteLine("Es mayor a 10");
+break;
+
+
+
+}
