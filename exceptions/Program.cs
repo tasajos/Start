17939b319998890c0cd6eadb0db1b35599668ed2
Carlos.Azcarraga <tasajos@gmail.com>
2022-11-17 15:42:45 -0400

    exceptions

---
+++ exceptions/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+ int x = 100;
+            int y = 10;
+            int result;
+
+            try {
+                if (x > 1000) {
+                    throw new ArgumentOutOfRangeException("x","x has to be 1000 or less");
+                }
+                result = x / y;
+                Console.WriteLine("The result is: {0}", result);
+            }
+            catch (DivideByZeroException e) {
+                Console.WriteLine("Whoops!");
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e) {
+                Console.WriteLine("Sorry, 1000 is the limit");
+                Console.WriteLine(e.Message);
+            }
+            finally {
+                Console.WriteLine("This code always runs.");
+            }
