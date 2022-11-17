9f0a6980498cb9f919c0d080d0a0705fcdc1754e
Carlos.Azcarraga <tasajos@gmail.com>
2022-11-17 14:08:14 -0400

    conditional

---
+++ Conditionals/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = 0;
+int b =4;
+
+// string
+
+Console.WriteLine("Por favor introduzca un numero");
+a = Convert.ToInt32(Console.ReadLine());
+
+
+if(a>b){
+
+    Console.WriteLine("Es Mayor");
+}
+else{
+    Console.WriteLine("Es Menor");
+}
+
+Console.WriteLine("Hello, World!");
+
+// INT
+//age = Convert.ToInt32(Console.ReadLine());
