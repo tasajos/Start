0847bf3eb44cb95e62f509f31b03bde71ab6b222
Carlos.Azcarraga <tasajos@gmail.com>
2022-11-17 11:14:00 -0400

    operators

--- operators/Program.cs
+++ operators/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+ // Declare some variables to excercise the operators
+            int x=10, y=5;
+            string a="abcd", b="efgh";
+
+            // Basic math operators are +, -, /, *
+            Console.WriteLine("----- Basic Math -----");
+            Console.WriteLine((x / y) * x);
+            Console.WriteLine(a + b);
+
+            // Increment / decrement operators
+            Console.WriteLine("----- Shorthand -----");
+            x++;
+            y--;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            // Operators can be shorthand: a = a + b
+            a += b;
+            Console.WriteLine(a);
+
+            // Logical operators &&, ||
+            Console.WriteLine("----- Logic Operators -----");
+            Console.WriteLine(x > y && y >= 5);
+            Console.WriteLine(x > y || y >= 5);
+
+            // null-coalescing operators
+            string str = null;
+            // the ?? operator uses left operand if not null, or right one if it is
+            Console.WriteLine(str ?? "Unknown string");
+            
+            // the ??= operator assigns the right operand if the left one is null
+            // it replaces the code:
+            // if (variable is null) {
+            //    variable = somevalue;
+            // }
+            str ??= "New string";
+            Console.WriteLine(str);
