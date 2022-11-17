ce9e1fabe5e49ce810600438900aab26e82d2281
Carlos.Azcarraga <tasajos@gmail.com>
2022-11-17 11:13:32 -0400

    operators

--- DataType/Program.cs
+++ DataType/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+/* Declaring variables */
+int i = 10;
+float f = 2.0f;
+decimal d = 10.0m;
+bool b = true;
+char c = 'c';
+
+  // Declare a string - it's a collection of characters
+ string str = "a string";
+// Declare an implicit variable
+var x = 10;
+var z = "Hello!";
+
+
 Console.WriteLine("Hello, World!");
